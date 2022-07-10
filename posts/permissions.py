995589d01aea13514944c120473bab10c4c50bea
artem <neznajkinartem357@gmail.com>
2022-07-10 18:21:11 +0600

    permission changed

--- posts/permissions.py
+++ posts/permissions.py
@@ -5,10 +5,10 @@ class IsCreatorPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
         if view.action == "create":
-            return request.user.is_authenticated()
+            return request.user.is_authenticated
         return True
 
     def has_object_permission(self, request, view, obj):
-        if view.action == "update" or view.action == "destroy":
+        if view.action == "update" or view.action == "destroy" or view.action == "partial_update":
             return obj.creator == request.user
         return True
