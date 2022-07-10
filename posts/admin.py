e5f1c22cebe3c00266ef86b73bc8d3f63c24f439
artem <neznajkinartem357@gmail.com>
2022-07-10 18:54:51 +0600

    comment to admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from posts.models import Post
+from posts.models import Post, Comment
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
     pass
