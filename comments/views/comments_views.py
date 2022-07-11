d3d125bc6645e8d58a1095f7a41fb2168adbf570
Artem <neznajkinartem357@gmail.com>
2022-07-11 11:07:51 +0600

    comment permissions

--- comments/views/comments_views.py
+++ comments/views/comments_views.py
@@ -3,12 +3,13 @@ from rest_framework import permissions
 
 from comments.models import Comment
 from comments.serializers.comments_serializers import CommentSerializer
+from comments.permissions import IsCreatorPermission
 
 
 class CommentsViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsCreatorPermission]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
