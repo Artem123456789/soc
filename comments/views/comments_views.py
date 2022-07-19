d39565676837508b54673ed93dfb170d7a7dacae
Artem <neznajkinartem357@gmail.com>
2022-07-19 17:57:53 +0600

    comment changes

--- comments/views/comments_views.py
+++ comments/views/comments_views.py
@@ -1,5 +1,4 @@
 from rest_framework import viewsets, status
-from rest_framework import permissions
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
